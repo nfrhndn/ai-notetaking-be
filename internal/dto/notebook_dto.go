ffe220d768b889e2c123e195306e50cb17ecb8d7
nfrhndn <nfrhndn711@gmail.com>
2026-02-02 14:21:45 +0700

    Membuat API Update Notebook

--- internal/dto/notebook_dto.go
+++ internal/dto/notebook_dto.go
@@ -14,11 +14,19 @@ type CreateNotebookRequest struct {
 type CreateNotebookResponse struct {
 	Id uuid.UUID `json:"id"`
 }
-
 type ShowNotebookResponse struct {
 	Id        uuid.UUID  `json:"id"`
 	Name      string     `json:"name"`
 	ParentId  *uuid.UUID `json:"parent_id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
+}
+
+type UpdateNotebookRequest struct {
+	Id   uuid.UUID
+	Name string `json:"name" validate:"required"`
+}
+
+type UpdateNotebookResponse struct {
+	Id uuid.UUID `json:"id"`
 }
--- internal/repository/notebook_repository.go
+++ internal/repository/notebook_repository.go
@@ -16,6 +16,7 @@ type INotebookRepository interface {
 	UsingTx(ctx context.Context, tx database.DatabaseQueryer) INotebookRepository
 	Create(ctx context.Context, notebook *entity.Notebook) error
 	GetById(ctx context.Context, id uuid.UUID) (*entity.Notebook, error)
+	Update(ctx context.Context, notebook *entity.Notebook) error
 }
 
 type notebookRepository struct {
@@ -71,6 +72,31 @@ func (n *notebookRepository) GetById(ctx context.Context, id uuid.UUID) (*entity
 		return nil, err
 	}
 	return &notebook, nil
+}
+
+func (n *notebookRepository) Update(ctx context.Context, notebook *entity.Notebook) error {
+	cmdTag, err := n.db.Exec(
+		ctx,
+		`UPDATE notebook SET 
+			name = $1,
+			parent_id = $2,
+			updated_at = $3
+		WHERE id = $4 AND is_deleted = false`,
+		notebook.Name,
+		notebook.ParentId,
+		notebook.UpdatedAt,
+		notebook.Id,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if cmdTag.RowsAffected() == 0 {
+		return serverutils.ErrNotFound
+	}
+
+	return nil
 }
 
 func NewNotebookRepository(db *pgxpool.Pool) INotebookRepository {
